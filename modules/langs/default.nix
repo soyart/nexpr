3d6a8a42917d175e82207ae3545907e380ef2291
soyart <artdrawin@gmail.com>
2024-05-06 00:48:01 +0700

    mod(langs)

--- modules/langs/default.nix
+++ modules/langs/default.nix
@@ -1,7 +1,30 @@
-{ ... }:
+{ lib, ... }:
 
-{
+with lib;
+with lib.types;
+
+let
+  mod = {
+    options = {
+      enable = mkEnableOption "Enable language support";
+      systemPackage = mkEnableOption "Enable language support as system package";
+    };
+  };
+
+in {
   imports = [
     ./go
+    ./rust
   ];
+
+  options.nexpr.langs = mkOption {
+      description = "Programming languages to install. See subdirectories of this module for supported languages";
+      type = attrsOf (submodule mod);
+      default = {
+        enable = false;
+        systemPackage = false;
+      };
+  };
+
+  # @TODO: Config simple languages here
 }
--- modules/langs/go/default.nix
+++ modules/langs/go/default.nix
@@ -8,21 +8,6 @@ let
   goBase = with pkgs; [ go gopls ];
 
 in {
-  options = {
-    nexpr.langs.go = {
-      enable = mkEnableOption ''
-        Enable Go language support.
-        This option will install `go` and `gopls` to user home-manager.
-      '';
-
-      systemPackage = mkEnableOption ''
-        Enable Go language support.
-        This option will add `go` and `gopls` to system packages instead of home-manager.
-        You must set enable = true before setting systemPackage = true.
-      '';
-    };
-  };
-
   config = mkIf cfg.enable {
     environment.systemPackages =
       mkIf cfg.systemPackage goBase;
